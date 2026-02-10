3b91ebb6a93fb2a131844f96adab34a902a4526f
zizo-2 <nassiriziad12@gmail.com>
2026-02-11 00:32:43 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("mousemove", (e) => {
-    const x = (e.clientX / window.innerWidth) * 100;
-    const y = (e.clientY / window.innerHeight) * 100;
-
-    document.body.style.background = `
-        radial-gradient(circle at ${x}% ${y}%, #00ff88, #06150e)
-    `;
-});
